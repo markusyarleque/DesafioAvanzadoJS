8126a22ccc82e5b13b58c1fdcbe18d09a568017f
MarkusYarleque <markus.yarleque@gmail.com>
2023-01-06 21:51:25 -0500

    Obra de arte

--- 4.obra_arte/README.md
+++ 4.obra_arte/README.md
@@ -1,2 +1,2 @@
-# TIEMPO RESTANTE
+# UNA OBRA DE ARTE
 > En este desafío, se propone crear una `clase` que almacene los `atributos` de una imagen, previamente subida por un enlace en `index.html`, además de tener un `método` que permita cargar el archivo `imagen` desde una fuente externa. El objetivo es `instanciar` la clase y `mostrar el resultado` al usuario.
--- 4.obra_arte/js/index.js
+++ 4.obra_arte/js/index.js
@@ -1,5 +1,5 @@
 "use strict";
-var img, rutaImagen, mostrarImagen, btnImagen;
+var img, rutaImagen, mostrarImagen, btnImagen, validarImagen;
 rutaImagen = document.getElementById('ruta');
 btnImagen = document.getElementById('btnMostrarImagen');
 mostrarImagen = document.getElementById('MostrarImagen');
@@ -19,6 +19,12 @@ class Imagen{
 
 btnImagen.addEventListener('click', ()=>{
     if(!rutaImagen.value) return;
+    validarImagen = rutaImagen.value.slice(-3);
+    console.log(validarImagen != 'gif');
+    if(validarImagen != 'jpg' && validarImagen != 'gif' && validarImagen != 'png'){
+        alert('Solo se admiten archivos de imagen: jpg - png - gif');
+        return;
+    }
     const imagen = new Imagen(rutaImagen.value);
     imagen.cargarImagen();
 });
